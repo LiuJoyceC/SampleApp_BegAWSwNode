8de1af1be46d29dcfc5f502f64e4c107f4cd1790
LiuJoyceC <liujoycec@gmail.com>
2016-01-08 12:47:56 -0800

    Modified so that if testing on localhost, the port will be 3000, otherwise defer to process.env

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use(function(err, req, res, next) {
 });
 
 
-app.set('port', globals.applicationPort);
+app.set('port', process.env.PORT || globals.applicationPort);
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
